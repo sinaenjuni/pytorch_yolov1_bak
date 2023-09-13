31574411da5c6ca8def937cc71b20b8f2137f74b
sinaenjuni <y2k4388@gmail.com>
2023-09-13 19:13:24 +0900

    mod annotation

--- utils/iou.py
+++ utils/iou.py
@@ -7,13 +7,13 @@ def intersection_over_union(box_preds, box_targets):
     maxx = torch.min(box_preds[..., 2], box_targets[..., 2])
     maxy = torch.min(box_preds[..., 3], box_targets[..., 3])
     intersection = (maxx - minx).clamp(0) * (maxy - miny).clamp(0)
-    print(intersection)
+    # print(intersection)
 
     # w: maxx - minx, h:maxy - miny
     preds_area =   torch.abs((box_preds[..., 2] - box_preds[..., 0]) * (box_preds[..., 3] - box_preds[..., 1]))
     targets_area = torch.abs((box_targets[..., 2] - box_targets[..., 0]) * (box_targets[..., 3] - box_targets[..., 1]))
     union = (preds_area + targets_area - intersection + 1e-6)
-    print(union)
+    # print(union)
     return intersection / union
 
 if __name__ == "__main__":
